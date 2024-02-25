04c0ad6f823ec9c599b284351ce0be7f8a6ea236
Siarhei Yurevich <s.yurevich@andersenlab.com>
2024-02-25 21:07:19 +0300

    init project

---
+++ src/test/java/com/cicd/jenkinstesting/JenkinsTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.jenkinstesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
